7821f755e592cd0f769e819ddb5e17b02b65a903
Habib Ali Machpud <habib.machpud@gmail.com>
2025-04-22 22:58:49 +0700

    add default action (print usage)

--- src/main/java/io/github/bluething/playground/java/Main.java
+++ src/main/java/io/github/bluething/playground/java/Main.java
@@ -1,7 +1,21 @@
 package io.github.bluething.playground.java;
 
 public class Main {
+    private static void printUsage() {
+        var usage = """
+                Usage:
+                add <description>                   Add new task
+                update <id> <description>           Update task description
+                delete <id>                         Delete task description
+                mark <id> <todo|in-progress|done>    Change task status
+                list [all|todo|in-progress|done]     List tasks
+                """;
+        System.out.println(usage);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length == 0) {
+            printUsage();
+            return;
+        }
     }
 }
